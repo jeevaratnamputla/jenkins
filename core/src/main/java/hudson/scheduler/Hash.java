b3654369bcd3d5bd66e2e97e1eff7288f03d2a77
jeevaratnamputla <132266626+jeevaratnamputla@users.noreply.github.com>
2026-07-22 11:16:33 +0530

    fix: use-of-md5-82 (#26)
    Co-authored-by: appuser <appuser@ai-guardian-remediation-9f4b65bdf-pkspv>

--- core/src/main/java/hudson/scheduler/Hash.java
+++ core/src/main/java/hudson/scheduler/Hash.java
@@ -53,9 +53,9 @@ public abstract class Hash {
     @SuppressFBWarnings(value = "PREDICTABLE_RANDOM", justification = "The random is just used for load distribution.")
     public static Hash from(String seed) {
         try {
-            MessageDigest md5 = getMd5();
-            md5.update(seed.getBytes(StandardCharsets.UTF_8));
-            byte[] digest = md5.digest();
+            MessageDigest sha256 = getSha256();
+            sha256.update(seed.getBytes(StandardCharsets.UTF_8));
+            byte[] digest = sha256.digest();
 
             for (int i = 8; i < digest.length; i++)
                 digest[i % 8] ^= digest[i];
@@ -72,14 +72,12 @@ public abstract class Hash {
                 }
             };
         } catch (NoSuchAlgorithmException e) {
-            throw new AssertionError(e);    // MD5 is a part of JRE
+            throw new AssertionError(e);    // SHA-256 is a part of JRE
         }
     }
 
-    // TODO JENKINS-60563 remove MD5 from all usages in Jenkins
-    @SuppressFBWarnings(value = "WEAK_MESSAGE_DIGEST_MD5", justification = "Should not be used for security.")
-    private static MessageDigest getMd5() throws NoSuchAlgorithmException {
-        return MessageDigest.getInstance("MD5");
+    private static MessageDigest getSha256() throws NoSuchAlgorithmException {
+        return MessageDigest.getInstance("SHA-256");
     }
 
     /**
